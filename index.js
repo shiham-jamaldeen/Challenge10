1da33c61cd678abdfb9c60b998d80d22d4a268c1
shiham-jamaldeen <112249220+shiham-jamaldeen@users.noreply.github.com>
2022-11-12 00:23:07 +1030

    added html template draft and index.js draft

---
+++ index.js
@@ -0,0 +1,17 @@
+//include packages needed for the application
+
+//inquirer
+const inquirer = require("inquirer");
+//file server
+const fileToWrite = require("fs");
+//html template file
+const generateHTML = require("./src/generateTemplate.js");
+
+function writeToFile(filename, data) {
+  //function writes data to html file
+}
+function generateQuestions() {
+  //function accepts user input
+}
+//main
+generateQuestions();
