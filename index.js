252c7fe7f44c9efa36a49e04828deb54cff239e9
shiham-jamaldeen <112249220+shiham-jamaldeen@users.noreply.github.com>
2022-11-13 19:49:15 +1030

    added manager,engineer and intern response function. Also added intern print to html function. Code now in working condition

--- index.js
+++ index.js
@@ -14,7 +14,9 @@ const engineer = require("./lib/engineer.js");
 const intern = require("./lib/intern.js");
 
 //global variables
-let engineerArray = []; //store engineer data array
+
+const engineerArray = [];
+const internArray = [];
 
 function writeToFile(fileName, data) {
   //function writes data to html file
@@ -26,34 +28,173 @@ function writeToFile(fileName, data) {
         )
   );
 }
-function generateQuestions() {
-  //function accepts user input
-  const mgrData = new manager(
-    "John",
-    "0001",
-    "john@company.com.au",
-    "08 1234 4567"
-  );
-
-  let engineerData = new engineer(
-    "Alec",
-    "0002",
-    "alec@company.com.au",
-    "alec-regusa"
-  );
-  engineerArray.push(engineerData);
-
-  let engineerData1 = new engineer(
-    "Grace",
-    "0004",
-    "grace@company.com.au",
-    "grace-lord"
-  );
-  engineerArray.push(engineerData1);
-
-  const data = generateHTML(mgrData, engineerArray);
+function processInfomation(mngrData, engineerArray, internArray) {
+  //call the function pass input from user to generate template file
+  const writeData = generateHTML(mngrData, engineerArray, internArray);
   const fileName = "./dist/generatedHTML.html";
-  writeToFile(fileName, data);
+  //call the function to write the template to a physical HTML file
+  writeToFile(fileName, writeData);
+}
+function getManagerResponses() {
+  //function accepts user input
+  //Manager Responses
+
+  inquirer
+    .prompt([
+      {
+        type: "input",
+        message: "Enter the manager's Name (Required):",
+        name: "empName",
+      },
+      {
+        type: "input",
+        message: "Enter the manager's Employee ID (Required):",
+        name: "id",
+      },
+      {
+        type: "input",
+        message: "Enter the manager's Email Address (Required):",
+        name: "email",
+      },
+      {
+        type: "input",
+        message: "Enter the manager's phone number:",
+        name: "officeNumber",
+      },
+      {
+        type: "list",
+        message: "What data would you like to enter?",
+        choices: ["Engineer", "Intern"],
+        name: "userChoice",
+      },
+    ])
+    .then((mngrResponses) => {
+      //write answers to manager object
+      let mgrData = new manager(
+        mngrResponses.empName,
+        mngrResponses.id,
+        mngrResponses.email,
+        mngrResponses.officeNumber
+      );
+
+      //check for user input
+      if (mngrResponses.userChoice === "Engineer") {
+        //call Engineer Responses
+        console.log("***Enter Engineer details***");
+        getEngineerResponses(mgrData);
+      } else if (mngrResponses.userChoice === "Intern") {
+        //call Intern Responses
+        console.log("***Enter Intern/Trainee details***");
+        getInternResponses(mgrData);
+      }
+    });
+}
+function getEngineerResponses(mgrData) {
+  inquirer
+    .prompt([
+      {
+        type: "input",
+        message: "Enter the engineer's Name (Required):",
+        name: "empName",
+      },
+      {
+        type: "input",
+        message: "Enter the engineer's Employee ID (Required):",
+        name: "id",
+      },
+      {
+        type: "input",
+        message: "Enter the engineer's Email Address (Required):",
+        name: "email",
+      },
+
+      {
+        type: "input",
+        message: "Enter the engineer's GitHub handle:",
+        name: "gitHub",
+      },
+    ])
+    .then((engineerResponses) => {
+      //initialise the Engineer object and assign responses
+      let engineerData = new engineer(
+        engineerResponses.empName,
+        engineerResponses.id,
+        engineerResponses.email,
+        engineerResponses.gitHub
+      );
+      //write the data to an array
+      engineerArray.push(engineerData);
+      console.log(engineerArray);
+      //call  user choice function, to check what user whats to do next
+      userChoice(mgrData);
+    });
+}
+function userChoice(mgrData) {
+  inquirer
+    .prompt([
+      {
+        type: "list",
+        message: "What data would you like to enter?",
+        choices: ["Engineer", "Intern", "Exit"],
+        name: "userChoice",
+      },
+    ])
+    .then((userResposnses) => {
+      if (userResposnses.userChoice === "Engineer") {
+        //call Engineer Responses
+        console.log("***Enter Engineer details***");
+        getEngineerResponses(mgrData);
+      } else if (userResposnses.userChoice === "Intern") {
+        //call Intern Responses
+        console.log("***Enter Intern/Trainee details***");
+        getInternResponses(mgrData);
+      } else if (userResposnses.userChoice === "Exit") {
+        console.log("Thank you! Your responses will be now saved!");
+        //console.log(managerArray);
+        //console.log(engineerArray);
+        processInfomation(mgrData, engineerArray, internArray);
+        return;
+      }
+    });
+}
+function getInternResponses(mgrData) {
+  inquirer
+    .prompt([
+      {
+        type: "input",
+        message: "Enter the trainee's Name (Required):",
+        name: "empName",
+      },
+      {
+        type: "input",
+        message: "Enter the trainees's Employee ID (Required):",
+        name: "id",
+      },
+      {
+        type: "input",
+        message: "Enter the trainee's Email Address (Required):",
+        name: "email",
+      },
+
+      {
+        type: "input",
+        message: "Enter the trainee's school:",
+        name: "school",
+      },
+    ])
+    .then((internResponses) => {
+      let internData = new intern(
+        internResponses.empName,
+        internResponses.id,
+        internResponses.email,
+        internResponses.school
+      );
+      //write data to an array
+      internArray.push(internData);
+      //call function to accept user choice
+      userChoice(mgrData);
+    });
 }
 //main
-generateQuestions();
+getManagerResponses();
+//processInfomation(managerArray, engineerArray);
--- src/generateTemplate.js
+++ src/generateTemplate.js
@@ -20,7 +20,7 @@ function printManagerCard(empName, id, email, contactNumber, role) {
     <li class="list-group-item">ID: ${id}</li>
     <li class="list-group-item">Email: <a href="mailto:${email}">${email}</a></li>
     <li class="list-group-item">Phone:${contactNumber}</li>
-</ul>`;
+</ul></div>`;
   return managerCardHTML;
 }
 function printEngineerCards(engineerArray) {
@@ -49,16 +49,50 @@ function printEngineerCards(engineerArray) {
         <li class="list-group-item">Email: <a href="mailto:${
           engineerArray.email
         }">${engineerArray.email}</a></li>
-        <li class="list-group-item">Phone:${engineerArray.gitHubHandle}</li>
-    </ul>`;
+        <li class="list-group-item">Github:<a href='${engineerArray.getGitHub()}'>${engineerArray.getGitHub()}</a></li>
+    </ul> </div>`;
       //concatenate HTML liverals to another variable
       engineerHTMLContent += engineerCard;
-      console.log(engineerCard);
-      return engineerHTMLContent;
     });
   }
+
+  //return the final variable with literals
+  return engineerHTMLContent;
 }
-function generateMainHTML(mgrData, engineerArray) {
+function printInternCards(internArray) {
+  let internHTMLContent = "";
+  if (internArray.length) {
+    internArray.forEach((internArray) => {
+      const internCard = `<div
+      class="card"
+      style="
+        width: 18rem;
+        border: 5px darkblue;
+        border-style: dashed;
+        box-shadow: 10px 10px lightskyblue;
+      "
+    >
+      <div class="card-body">
+        <h3 class="card-title">${internArray.empName}</h3>
+        <h4 class="card-title">
+          <i class="fa fa-html5" style="font-size: 36px; color: blue"></i>
+          ${internArray.getRole()}
+        </h4>
+      </div>
+      <ul class="list-group list-group-flush">
+        <li class="list-group-item">ID: ${internArray.id}</li>
+        <li class="list-group-item">Email: <a href="mailto:${
+          internArray.email
+        }">${internArray.email}</a></li>
+        <li class="list-group-item">School:${internArray.school}</a></li>
+    </ul> </div>`;
+      //concatenate HTML liverals to another variable
+      internHTMLContent += internCard;
+    });
+  }
+  return internHTMLContent;
+}
+function generateMainHTML(mgrData, engineerArray, internArray) {
   const mainHTMLCode = `<!DOCTYPE html>
     <html>
       <head>
@@ -82,6 +116,7 @@ function generateMainHTML(mgrData, engineerArray) {
       <body>
         <header class="header jumbotron"><h1>My Team</h1></header>
         <main>
+        <div class="container">
         <div class="d-flex justify-content-between">
         
   ${printManagerCard(
@@ -92,7 +127,23 @@ function generateMainHTML(mgrData, engineerArray) {
     mgrData.getRole()
   )}
   ${printEngineerCards(engineerArray)}
-`;
+  ${printInternCards(internArray)}
+  </div>
+  </div>
+    </main>
+    <script
+      src="https://cdn.jsdelivr.net/npm/bootstrap@4.0.0/dist/js/bootstrap.min.js"
+      integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl"
+      crossorigin="anonymous"
+    ></script>
+    <script
+      src="https://cdn.jsdelivr.net/npm/popper.js@1.12.9/dist/umd/popper.min.js"
+      integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q"
+      crossorigin="anonymous"
+    ></script>
+  </body>
+</html>  
+  `;
   return mainHTMLCode;
 }
 module.exports = generateMainHTML;
